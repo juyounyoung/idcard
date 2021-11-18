19e0cfd8902e912b8752207a9c2135de51fa75cc
jyy <love5932@naver.com>
2021-11-18 17:57:47 +0900

    login.html login_style.html board_List.html board.css

--- idcard/urls.py
+++ idcard/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login, name='login'),
-    path('/board_list',views.board_list, name='board_list'),
+    path('board_list',views.board_list, name='board_list'),
 
 ]
